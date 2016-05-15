e885c5009daed1932828350321078d4b28f8faf0
Adde A.Nilsson <adde.a.nilsson@gmail.com>
2016-05-15 14:16:46 +0200

    experimenting mobile touchstart effect

--- js/main.js
+++ js/main.js
@@ -2,4 +2,7 @@ $(document).ready(function() {
 	$('#trigger').on('click', function() {
 		$('.mobile-toggle').slideToggle(600);
 	});
+	$('.index li a').on('touchstart', function() {
+		$(this).css({backgroundColor: '#202020', color: '#fff'});
+	});
 });
