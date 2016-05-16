1112a1af779436fcf0ac28065d242cc57291a4ea
Adde A.Nilsson <adde.a.nilsson@gmail.com>
2016-05-16 10:22:03 +0200

    added touch effects for menu-links and landingpage btns. wrapped page article in col

--- js/main.js
+++ js/main.js
@@ -8,4 +8,22 @@ $(document).ready(function() {
 	$('.index li a').on('touchend', function() {
 		$(this).css({backgroundColor: 'transparent', color: '#333'});
 	});
+	$('.custom-btn').on('touchstart', function() {
+		$(this).css({background: 'transparent', color: '#202020'});
+	});
+	$('.custom-btn').on('touchend', function() {
+		$(this).css({background: '#202020', color: '#f0f0f0'});
+	});
+	$('nav a').on('touchstart', function() {
+		$(this).css({backgroundColor: '#202020', color: '#fff'});
+	});
+	$('nav a').on('touchend', function() {
+		$(this).css({backgroundColor: 'transparent', color: '#333'});
+	});
+	$('.home-link').on('touchstart', function() {
+		$(this).css({borderBottom: '1px solid #202020'});
+	});
+	$('.home-link').on('touchend', function() {
+		$(this).css({borderBottom: 'none'});
+	});
 });
